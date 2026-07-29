87ca33d02daa9fb06038b132a3e67bca699ef2e8
Zzz7v615 <3137627181@qq.com>
2026-07-29 09:41:38 +0800

    Bump cache to v3

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker - 公考财经自律管理工作台
 // 实现离线缓存：首次访问后，无网络也能打开
-const CACHE_NAME = 'gongkao-dashboard-v2';
+const CACHE_NAME = 'gongkao-dashboard-v3';
 const ASSETS = [
   './',
   './index.html',
